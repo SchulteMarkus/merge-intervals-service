cb24e8eefe465f7b616ed7b5655893f3666bcd67
Markus Schulte <markusschulte.work@gmail.com>
2021-07-08 14:29:21 +0200

    #3 Implement API endpoint for merging intervals

--- src/main/java/schulte/markus/service/intervals/IntervalsResource.java
+++ src/main/java/schulte/markus/service/intervals/IntervalsResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
@@ -19,19 +20,12 @@ public class IntervalsResource {
   @POST
   public List<IntegerInterval> mergeIntervals(
       @RequestBody(
-              description = "abc",
               content = {
                 @Content(
-                    example =
-                        "[{\"start\":0,\"end\":1},{\"start\"0,\"end\"2},{\"start\"2,\"end\"5}]")
+                  mediaType = MediaType.APPLICATION_JSON,
+                  example = "[{\"start\":1,\"end\":2},{\"start\":0,\"end\":2},{\"start\":3,\"end\":5}]")
               })
           final List<IntegerInterval> intervals) {
     return intervalsMerger.merge(intervals);
-  }
-
-  @Path("/test")
-  @POST
-  public List<IntegerInterval> returnIntervals(final List<IntegerInterval> intervals) {
-    return intervals;
   }
 }
